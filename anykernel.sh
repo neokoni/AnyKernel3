fb6a69f219b009fc2b7897b2423d4af227850b90
neokoni <dulin081682@126.com>
2025-04-27 17:36:33 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,17 +4,13 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=KernelSU by KernelSU Developers
+kernel.string=kernel flash
 do.devicecheck=0
 do.modules=0
-do.systemless=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=
-device.name2=
-device.name3=
-device.name4=
-device.name5=
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
@@ -23,21 +19,13 @@ supported.vendorpatchlevels=
 
 ### AnyKernel install
 ## boot shell variables
-block=boot
-is_slot_device=auto
+block=/dev/block/platform/soc/1d84000.ufshc/by-name/boot
+is_slot_device=1
 ramdisk_compression=auto
 patch_vbmeta_flag=auto
-no_magisk_check=1
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh
 
-# boot install
-if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
-    split_boot # for devices with init_boot ramdisk
-    flash_boot # for devices with init_boot ramdisk
-else
-    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
-fi
-## end boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
